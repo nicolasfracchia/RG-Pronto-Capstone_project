b897ae50c942f5fe0163177c8887a457fc0cef9c
nicolas.fracchia <nicolas.fracchia@freshwatercreative.ca>
2024-02-02 21:42:38 -0700

    Routes for pages home, menu and catering

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,16 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
   },
   {
+    path: 'menu',
+    loadComponent: () => import('./pages/menu/menu.page').then( m => m.MenuPage)
+  },
+  {
+    path: 'catering',
+    loadComponent: () => import('./pages/catering/catering.page').then( m => m.CateringPage)
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
   },
-  {
-    path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
-  }
 ];
