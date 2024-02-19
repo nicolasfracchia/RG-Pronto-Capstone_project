0a66b406e436955bda592f81f07fcdc6ba8e101e
nicolas.fracchia <nicolas.fracchia@freshwatercreative.ca>
2024-02-18 20:49:33 -0700

    Functionality to update the units counter on each product-price

--- src/app/pages/order/order.page.ts
+++ src/app/pages/order/order.page.ts
@@ -31,6 +31,8 @@ export class OrderPage implements OnInit {
   public arrayNumsForTesting1: number[];
   public arrayNumsForTesting2: number[];
 
+  private updatingUnits:boolean = false;
+
 
   constructor() {
    this.arrayNumsForTesting1 = Array.from({length: 15}, (_, i) => i);
@@ -40,4 +42,37 @@ export class OrderPage implements OnInit {
 
   ngOnInit() {
   }
+
+  addUnit(id:string){
+    this.updateUnits(id, 'add');
+  }
+
+  removeUnit(id:string){
+    this.updateUnits(id, 'remove');
+  }
+
+  updateUnits(id:string, type:string){
+    if(!this.updatingUnits){
+      this.updatingUnits = true;
+    
+      const element = document.getElementById(id);
+    
+      if(element){
+        const q = parseInt(element.innerHTML);
+        let newQ = 0;
+        
+        if(type === 'add'){
+          newQ = q + 1;
+        }else if (type === 'remove'){
+          newQ = (q === 0) ? 0 : q - 1;
+        }
+
+        element.innerHTML = newQ.toString();
+      }
+      
+      this.updatingUnits = false;
+    }
+  }
+
+
 }
