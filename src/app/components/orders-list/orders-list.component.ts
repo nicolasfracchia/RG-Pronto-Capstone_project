68e12414717a74648cc256687eb6bc6bf5b1492a
nicolas.fracchia <nicolas.fracchia@freshwatercreative.ca>
2024-02-25 12:46:39 -0700

    orders list component created and applied in home

--- src/app/components/orders-list/orders-list.component.ts
+++ src/app/components/orders-list/orders-list.component.ts
@@ -1,18 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { Order } from 'src/app/interfaces/order';
+import { IonList, IonItem, IonGrid, IonRow, IonCol, IonLabel, IonItemGroup, IonContent, IonNote } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-orders-list',
   templateUrl: './orders-list.component.html',
   styleUrls: ['./orders-list.component.scss'],
   standalone: true,
-  imports: [
-    CommonModule
+  imports: [IonNote, IonContent, IonItemGroup, 
+    CommonModule,
+    IonList,
+    IonItem,
+    IonLabel,
   ]
 })
 export class OrdersListComponent  implements OnInit {
   @Input() orders!:Order[];
+  public colors:string[] = ['', 'medium', 'secondary', 'primary', 'warning', 'success', 'danger'];
+      // Waiting for approval - Confirmed - In progress - On the way - Delivered - Cancelled
+  
 
   constructor() { }
 
