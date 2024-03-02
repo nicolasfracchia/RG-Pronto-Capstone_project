dc72df2019309f84a8301627288dfc1818ebe0d2
nicolas.fracchia <nicolas.fracchia@freshwatercreative.ca>
2024-03-02 09:20:43 -0700

    Images working on component  for in-store menu and catering sections

--- src/app/components/products-by-section/products-by-section.component.ts
+++ src/app/components/products-by-section/products-by-section.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem, IonLabel, IonList, IonThumbnail ,IonText ,IonGrid ,IonRow ,IonCol } from '@ionic/angular/standalone';
 import { ProductsBySection } from 'src/app/interfaces/products-by-section';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-products-by-section',
@@ -26,10 +27,14 @@ export class ProductsBySectionComponent implements OnInit{
 
   @Input() section!: ProductsBySection;
 
+  foodImagesURL:string = environment.food_images_URL;
+  defaultImageURL:string = environment.default_food_image_URL;
+
   constructor() { 
     
   }
 
   ngOnInit() { }
+
 
 }
