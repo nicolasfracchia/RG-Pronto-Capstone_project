aba85ab7ca8c1f91bbf2df74218ed77d22a4ff19
nicolas.fracchia <nicolas.fracchia@freshwatercreative.ca>
2024-03-02 09:10:51 -0700

    environment variables for images and back end public route

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiURL: 'https://prontoapi.nicolasfracchia.com/'
+  apiURL: 'https://prontoapi.nicolasfracchia.com/',
+  public_folder_URL: 'https://prontoapi.nicolasfracchia.com/public/',
+  food_images_URL: 'https://prontoapi.nicolasfracchia.com/public/images/products/',
+  default_food_image_URL: 'assets/images/food/default.png',
 };
